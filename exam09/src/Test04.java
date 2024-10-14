5d8ac0ce8b30f80459278e0f9a91fbbb85fef376
cjduri <cjduri@kakao.com>
2024-10-14 17:06:43 +0900

    modify & correct

--- exam09/src/Test04.java
+++
@@ -1,8 +0,0 @@
-public class Test04 {
-    public static void main(String[] args) {
-        int[] a = new int[5];
-        System.out.println("a = " + a);
-        a = null;
-        System.out.println("a = " + a);
-    }
-}
